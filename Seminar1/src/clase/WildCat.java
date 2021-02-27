a6360934f751230b844b1b907159e61273b86c86
Rares <poparares18@stud.ase.ro>
2021-02-27 11:21:47 +0200

    Adaugare clasa WildCat

---
+++ Seminar1/src/clase/WildCat.java
@@ -0,0 +1,16 @@
+package clase;
+
+public class WildCat extends Animal {
+	private String mancare;
+
+	public WildCat(String nume) {
+		super(nume);
+		// TODO Auto-generated constructor stub
+	}
+
+	public WildCat(String nume, String mancare) {
+		super(nume);
+		this.mancare = mancare;
+	}
+
+}
--- Seminar1/src/seminar1/Main.java
+++ Seminar1/src/seminar1/Main.java
@@ -2,16 +2,24 @@ package seminar1;
 
 import clase.Animal;
 import clase.Lion;
+import clase.WildCat;
 import clase.Zoo;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Zoo zoo=new Zoo();
+		
 		Animal lion1=new Lion("Leo");
 		Lion lion2=new Lion("Leona");
 		zoo.addAnimal(lion1);
 		zoo.addAnimal(lion2);
+		zoo.feedAllAnimals();
+		
+		WildCat cat1=new WildCat("pisica1");
+		WildCat cat2=new WildCat("pisica2","mancare2");
+		zoo.addAnimal(cat1);
+		zoo.addAnimal(cat2);
 		zoo.feedAllAnimals();
 	}
 
